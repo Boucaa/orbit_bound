b8c6a3e6d6e48b2e066a5c590fdab1bb97432713
David Vokrouhlicky <david.vokrouhlicky@gmail.com>
2023-04-30 13:30:27 +0200

    RK4 integrator, added target object

--- lib/business/game_bloc.dart
+++ lib/business/game_bloc.dart
@@ -37,7 +37,7 @@ class GameBloc extends Bloc<GameEvent, GameState> {
               NewtonObject(
                 position: Vector2(200, 500),
                 velocity: Vector2.zero(),
-                mass: 10000,
+                mass: 100,
               ),
             ],
             lastTick: DateTime.now(),
@@ -56,9 +56,10 @@ class GameBloc extends Bloc<GameEvent, GameState> {
       emit(state.copyWith(isRunning: false));
     });
     on<Tick>(
-      (event, emit) {
+          (event, emit) {
         final time = DateTime.now();
         final timeDiff = time.difference(state.lastTick).inMicroseconds;
+        const double timeStep = 16*0.5;
         // _log.fine('Tick: $timeDiff');
         if (timeDiff > 30000) {
           _log.fine('long tick: $timeDiff');
@@ -67,8 +68,8 @@ class GameBloc extends Bloc<GameEvent, GameState> {
         final dx = objects[0].position.x - objects[1].position.x;
         final dy = objects[0].position.y - objects[1].position.y;
         final e = 0.5 *
-                (pow(objects[0].velocity.x, 2) -
-                    pow(objects[0].velocity.y, 2)) -
+            (pow(objects[0].velocity.x, 2) +
+                pow(objects[0].velocity.y, 2)) -
             10000 / pow(pow(dx, 2) + pow(dy, 2), 0.5);
         _log.fine('Energy: $e');
         final newObjects = <GameObject>[];
@@ -78,20 +79,49 @@ class GameBloc extends Bloc<GameEvent, GameState> {
             continue;
           }
           var acceleration = Vector2.zero();
-          for (var j = 0; j < objects.length; j++) {
-            if (i == j) {
-              continue;
+          Vector2 calcAcceleration(Vector2 testPosition){
+            for (var j = 0; j < objects.length; j++) {
+              if (i == j) {
+                continue;
+              }
+
+              final objectA = objects[i].copyWith(
+                position: testPosition,
+              );
+              final objectB = objects[j];
+
+              final interaction = objectB.calculateInteraction(objectA);
+              acceleration += interaction;
             }
-            final objectA = objects[i];
-            final objectB = objects[j];
-            final interaction = objectB.calculateInteraction(objectA);
-            acceleration += interaction;
+            return acceleration;
           }
-          final newVelocity =
-              objects[i].velocity + acceleration * (16000 * 0.000001);
+
+          // Calculate the k1 values
+          final k1Velocity = calcAcceleration(objects[i].position) * timeStep;
+          final k1Position = objects[i].velocity * timeStep;
+
+          // Calculate the k2 values
+          final k2Velocity = calcAcceleration(objects[i].position + k1Position * 0.5) * timeStep;
+          final k2Position = (objects[i].velocity + k1Velocity * 0.5) * timeStep;
+
+          // Calculate the k3 values
+          final k3Velocity = calcAcceleration(objects[i].position + k2Position * 0.5) * timeStep;
+          final k3Position = (objects[i].velocity + k2Velocity * 0.5) * timeStep;
+
+          // Calculate the k4 values
+          final k4Velocity = calcAcceleration(objects[i].position + k3Position) * timeStep;
+          final k4Position = (objects[i].velocity + k3Velocity) * timeStep;
+
+          // Update the velocity and position
+          final newVelocity = objects[i].velocity + (k1Velocity + k2Velocity * 2 + k3Velocity * 2 + k4Velocity) * (1 / 6);
+          final newPosition = objects[i].position + (k1Position + k2Position * 2 + k3Position * 2 + k4Position) * (1 / 6);
+
+
+          // final newVelocity =
+          //     objects[i].velocity + acceleration * (16000 * 0.000001);
           final newObject = objects[i].copyWith(
             velocity: newVelocity,
-            position: objects[i].position + newVelocity,
+            position: newPosition,//objects[i].position + newVelocity * (16000*0.000001),
           );
           newObjects.add(newObject);
         }
--- lib/model/target.dart
+++ lib/model/target.dart
@@ -1,40 +1,40 @@
-import 'dart:math';
-
 import 'package:space_balls/model/game_object.dart';
 import 'package:vector_math/vector_math_64.dart';
 
-class VariableGravityObject extends GameObject {
-  final double exponent;
-
-  VariableGravityObject({
+class target extends GameObject {
+  target({
     required Vector2 position,
     required Vector2 velocity,
     required double mass,
-    required this.exponent
-
   }) : super(
     position: position,
     velocity: velocity,
     mass: mass,
-    isStatic: true,
+    isStatic: false,
   );
 
   @override
   Vector2 calculateInteraction(GameObject other) {
-    double distance = sqrt(pow(other.position.x-position.x,2)+pow(other.position.y-position.y,2));
-    return Vector2(other.position.x-position.x,other.position.y-position.y)/pow(distance, exponent).toDouble()*mass;
+/*
+    return Vector2(
+          other.position.x - position.x,
+          other.position.y - position.y,
+        ).normalized() *
+        2;
+*/
+    return Vector2.zero();
   }
+
   @override
   GameObject copyWith({
     Vector2? position,
     Vector2? velocity,
     double? mass,
   }) {
-    return VariableGravityObject(
+    return target(
       position: position ?? this.position,
       velocity: velocity ?? this.velocity,
       mass: mass ?? this.mass,
-      exponent: exponent ?? this.exponent,
     );
   }
 }
