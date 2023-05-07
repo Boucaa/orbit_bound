d5ff40cc76e48a82269ec0e06f84cf0aac5a3cbb
David Vokrouhlicky <david.vokrouhlicky@gmail.com>
2023-05-07 21:48:37 +0200

    Added home page, levels page

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:space_balls/ui/game_page.dart';
+import 'package:space_balls/ui/levels_page.dart';
+
+import 'package:flutter/material.dart';
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/home.gif'),
+              const SizedBox(height: 20),
+              Text(
+                'Space balls',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LevelsPage()),
+                  );
+                },
+                child: const Text('Levels'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // TODO: Add functionality for the second button
+                },
+                child: const Text('Level editor'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // TODO: Add functionality for the third button
+                },
+                child: const Text('Educational portal'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/levels_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:space_balls/ui/game_page.dart';
+
+class LevelsPage extends StatelessWidget {
+  const LevelsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text('Levels Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Text(
+              'Select a Level',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                  childAspectRatio: 1,
+                ),
+                itemCount: 15,
+                itemBuilder: (context, index) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const GamePage()),
+                      );
+                    },
+                    child: Text(
+                      (index + 1).toString(),
+                      style: const TextStyle(fontSize: 24),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
