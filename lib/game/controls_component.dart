4128c5c74c14046fa96f7b537dbed26e25632bed
Jan Bouček <jan@janboucek.com>
2023-05-22 00:12:56 +0200

    new controls, new control visuals, cleanup

---
+++ lib/game/controls_component.dart
@@ -0,0 +1,157 @@
+import 'dart:async';
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
+import 'package:logging/logging.dart';
+
+class ControlsComponent extends PositionComponent with DragCallbacks {
+  final _log = Logger('ControlsComponent');
+
+  final Function(Vector2) onShoot;
+
+  Vector2? startPosition;
+
+  Vector2? get endPosition {
+    if (startPosition == null ||
+        endDevicePosition == null ||
+        startDevicePosition == null) {
+      return null;
+    }
+    final ratio = switch (startDevicePosition!.length) {
+      0 => 1.0, // avoid division by zero
+      double l => startPosition!.length / l,
+    };
+
+    return startPosition! - (startDevicePosition! - endDevicePosition!) * ratio;
+  }
+
+  // we need to use the device position, because the real canvas position is bugged for the update and end events
+  Vector2? startDevicePosition;
+  Vector2? endDevicePosition;
+  final Vector2 widgetStartOffset;
+
+  ControlsComponent({
+    required this.onShoot,
+    required super.size,
+    required this.widgetStartOffset,
+  }) : super(priority: 100);
+
+  @override
+  FutureOr<void> onLoad() {
+    add(
+      ShotPreviewComponent(
+        controlsComponent: this,
+      ),
+    );
+    return super.onLoad();
+  }
+
+  @override
+  void onDragStart(DragStartEvent event) {
+    startPosition = event.canvasPosition;
+    startDevicePosition = event.devicePosition - widgetStartOffset;
+    super.onDragStart(event);
+  }
+
+  @override
+  void onDragUpdate(DragUpdateEvent event) {
+    endDevicePosition = event.devicePosition - widgetStartOffset;
+    super.onDragUpdate(event);
+  }
+
+  @override
+  void onDragEnd(DragEndEvent event) {
+    if (startPosition != null &&
+        endDevicePosition != null &&
+        startDevicePosition != null) {
+      final force = startPosition! - endPosition!;
+      onShoot(force);
+      startPosition = null;
+      startDevicePosition = null;
+      endDevicePosition = null;
+    }
+    super.onDragEnd(event);
+  }
+}
+
+class ShotPreviewComponent extends Component {
+  final _log = Logger('ShotPreviewComponent');
+  final ControlsComponent controlsComponent;
+  final Random _random = Random();
+
+  ShotPreviewComponent({
+    required this.controlsComponent,
+  }) : super(priority: 100);
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    if (controlsComponent.startPosition == null ||
+        controlsComponent.endPosition == null) {
+      return;
+    }
+
+    final startPosition = controlsComponent.startPosition!.toOffset();
+    final endPosition = controlsComponent.endPosition!.toOffset();
+
+    if (startPosition == endPosition) {
+      return;
+    }
+
+    canvas.drawLine(
+      startPosition,
+      endPosition,
+      Paint()
+        ..color = const Color(0xFF00FF00) // The color of the photon trail.
+        ..strokeWidth =
+            0.03, // Adjust the thickness of the photon trail as needed.
+    );
+
+    // Calculate direction of the main line.
+    final direction = endPosition - startPosition;
+    final normalizedDirection = direction / direction.distance;
+
+    // Perpendicular vector to the main line.
+    final perpendicularVector =
+        Offset(-normalizedDirection.dy, normalizedDirection.dx);
+
+    // Draw particles along the line.
+    final particleCount = 30;
+    for (var i = 0; i < particleCount; ++i) {
+      final t = i / (particleCount - 1);
+      final position = Offset(
+        lerpDouble(startPosition.dx, endPosition.dx, t)!,
+        lerpDouble(startPosition.dy, endPosition.dy, t)!,
+      );
+
+      // Offset the particle position slightly from the main line.
+      final sideOffset =
+          perpendicularVector * (_random.nextDouble() * 0.06 - 0.03);
+
+      // Draw a small streak as the particle.
+      final offsetLength = _random.nextDouble() * 0.04;
+      final offset = normalizedDirection * offsetLength;
+      final particleStart = position + sideOffset - offset;
+      final particleEnd = position + sideOffset + offset;
+
+      // Generate a color that transitions from green to white.
+      final colorRnd = _random.nextInt(256);
+      final color = Color.fromRGBO(
+        colorRnd,
+        255,
+        colorRnd,
+        _random.nextDouble(), // alpha
+      );
+
+      canvas.drawLine(
+        particleStart,
+        particleEnd,
+        Paint()
+          ..strokeWidth = 0.02
+          ..color = color,
+      );
+    }
+  }
+}
--- lib/ui/flame_widget.dart
+++ lib/ui/flame_widget.dart
@@ -5,15 +5,13 @@ import 'package:logging/logging.dart';
 import 'package:space_balls/business/user_bloc.dart';
 import 'package:space_balls/game/space_balls_game.dart';
 import 'package:space_balls/model/game_level.dart';
-import 'package:space_balls/model/player_ball.dart';
 import 'package:space_balls/ui/game_page.dart';
-import 'package:space_balls/ui/preview_painter.dart';
-
-import '../business/game_bloc.dart';
 
 final _log = Logger('FlameWidget');
 
 class FlameWidget extends StatefulWidget {
+  static final gameKey = GlobalKey();
+
   final GameLevel level;
   final int levelId;
 
@@ -49,170 +47,107 @@ class _FlameWidgetState extends State<FlameWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<GameBloc, GameState>(
-      builder: (context, state) {
-        return GestureDetector(
-          onPanStart: (details) {
-            _log.fine(
-                'onPanStart: ${details.localPosition.dx} ${details.localPosition.dy}');
-            context.read<GameBloc>().add(
-                  StartPreview(details.localPosition),
-                );
-          },
-          onPanUpdate: (details) {
-            _log.fine(
-                'onPanUpdate: ${details.localPosition.dx} ${details.localPosition.dy}');
-            context.read<GameBloc>().add(
-                  PreviewShot(details.localPosition),
-                );
-          },
-          onPanEnd: (details) {
-            context.read<GameBloc>().add(
-                  Shoot(),
-                );
-            if (state.previewOffset != null) {
-              final player = game.level.gameObjects.firstWhere(
-                (element) => element is PlayerBall,
-              ) as PlayerBall;
-              final x = state.previewOffset!.dx - state.previewStart!.dx;
-              final y = state.previewOffset!.dy - state.previewStart!.dy;
-              game.shoot(Vector2(x, y));
-            } else {
-              _log.severe('onPanEnd: previewOffset is null');
-            }
-          },
-          child: Container(
-            color: const Color(0xFF2D2D2D),
-            child: Stack(
-              children: [
-                SafeArea(
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    child: FittedBox(
-                      fit: BoxFit.contain,
-                      alignment: Alignment.topCenter,
-                      child: SizedBox(
-                        width: 3,
-                        height: 3.0 * 16.0 / 9.0,
-                        child: GameWidget(game: game),
-                      ),
-                    ),
+    return Container(
+      color: const Color(0xFF2D2D2D),
+      child: Stack(
+        children: [
+          SafeArea(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: FittedBox(
+                fit: BoxFit.contain,
+                alignment: Alignment.topCenter,
+                child: SizedBox(
+                  width: 3,
+                  height: 3.0 * 16.0 / 9.0,
+                  child: GameWidget(
+                    game: game,
+                    key: FlameWidget.gameKey,
                   ),
                 ),
-                if (state.previewOffset != null)
-                  Positioned(
-                    top: 0,
-                    left: 0,
-                    child: _previewPainter(context, state),
-                  ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        FloatingActionButton(
-                          heroTag: 'back',
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          tooltip: 'Back',
-                          backgroundColor: Colors.deepOrange,
-                          child: const Icon(
-                            Icons.arrow_back,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(width: 8),
-                        FloatingActionButton(
-                          heroTag: 'reset',
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => GamePage(
-                                    levelId: widget.levelId,
-                                  ),
-                                ));
-                          },
-                          tooltip: 'Reset',
-                          backgroundColor: Colors.blue,
-                          child: const Icon(
-                            Icons.refresh,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(width: 8),
-                        FloatingActionButton(
-                          heroTag: 'pause',
-                          onPressed: () {
-                            if (game.paused) {
-                              setState(() {
-                                game.paused = false;
-                              });
-                            } else {
-                              setState(() {
-                                game.paused = true;
-                              });
-                            }
-                          },
-                          tooltip: 'pause',
-                          backgroundColor:
-                              !game.paused ? Colors.red : Colors.green,
-                          child: Icon(
-                            !game.paused ? Icons.pause : Icons.play_arrow,
-                            color: Colors.white,
-                          ),
-                        ),
-
-                        const Spacer(), // Add this to fill remaining space
-                        BlocBuilder<UserBloc, UserState>(
-                          builder: (context, state) {
-                            final user = state.user;
-                            if (user != null &&
-                                user.levelsCompleted.contains(widget.levelId)) {
-                              return const Icon(
-                                Icons.check,
-                                color: Colors.green,
-                                size: 36,
-                              );
-                            } else {
-                              return const SizedBox.shrink();
-                            }
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
+              ),
             ),
           ),
-        );
-      },
-    );
-  }
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  FloatingActionButton(
+                    heroTag: 'back',
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    tooltip: 'Back',
+                    backgroundColor: Colors.deepOrange,
+                    child: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  FloatingActionButton(
+                    heroTag: 'reset',
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => GamePage(
+                              levelId: widget.levelId,
+                            ),
+                          ));
+                    },
+                    tooltip: 'Reset',
+                    backgroundColor: Colors.blue,
+                    child: const Icon(
+                      Icons.refresh,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  FloatingActionButton(
+                    heroTag: 'pause',
+                    onPressed: () {
+                      if (game.paused) {
+                        setState(() {
+                          game.paused = false;
+                        });
+                      } else {
+                        setState(() {
+                          game.paused = true;
+                        });
+                      }
+                    },
+                    tooltip: 'pause',
+                    backgroundColor: !game.paused ? Colors.red : Colors.green,
+                    child: Icon(
+                      !game.paused ? Icons.pause : Icons.play_arrow,
+                      color: Colors.white,
+                    ),
+                  ),
 
-  CustomPaint _previewPainter(BuildContext context, GameState state) {
-    final playerBall = state.level.gameObjects.firstWhere(
-      (element) => element is PlayerBall,
-    ) as PlayerBall;
-    return CustomPaint(
-      painter: PreviewPainter(
-        [
-          Offset(
-            playerBall.position.x.roundToDouble() * 100,
-            playerBall.position.y.roundToDouble() * 100,
-          ),
-          Offset(
-            state.previewOffset!.dx -
-                state.previewStart!.dx +
-                playerBall.position.x * 100,
-            state.previewOffset!.dy -
-                state.previewStart!.dy +
-                playerBall.position.y * 100,
-          ),
+                  const Spacer(), // Add this to fill remaining space
+                  BlocBuilder<UserBloc, UserState>(
+                    builder: (context, state) {
+                      final user = state.user;
+                      if (user != null &&
+                          user.levelsCompleted.contains(widget.levelId)) {
+                        return const Icon(
+                          Icons.check,
+                          color: Colors.green,
+                          size: 36,
+                        );
+                      } else {
+                        return const SizedBox.shrink();
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
