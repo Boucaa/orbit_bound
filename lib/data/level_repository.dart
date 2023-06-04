b7c769495f1e4a4aae8d81bed6d048baa57b90a4
David Vokrouhlicky <david.vokrouhlicky@gmail.com>
2023-06-04 12:50:45 +0200

    Orbit level, titles

--- lib/data/level_repository.dart
+++ lib/data/level_repository.dart
@@ -1,6 +1,7 @@
 import 'package:space_balls/data/levels/black_hole_merger_level.dart';
 import 'package:space_balls/data/levels/kerr_level.dart';
 import 'package:space_balls/data/levels/newtonian_level.dart';
+import 'package:space_balls/data/levels/orbit_around_level.dart';
 import 'package:space_balls/data/levels/schwardschild_level.dart';
 import 'package:space_balls/data/levels/slalom_level.dart';
 import 'package:space_balls/data/levels/sun_with_planet_level.dart';
@@ -8,8 +9,10 @@ import 'package:space_balls/data/levels/test_level.dart';
 import 'package:space_balls/data/levels/tutorial_level.dart';
 import 'package:space_balls/data/levels/variable_gravity_level.dart';
 import 'package:space_balls/data/levels/wall_bounce_level.dart';
-import 'package:space_balls/model/game_level.dart';
 import 'package:space_balls/data/levels/negative_mass_level.dart';
+import 'package:space_balls/data/levels/orbit_around_level.dart';
+import 'package:space_balls/model/game_level.dart';
+
 
 class LevelRepository {
   static List<GameLevel Function()> get _levels => [
@@ -24,6 +27,7 @@ class LevelRepository {
         () => VariableGravityLevel(),
         () => SlalomLevel(),
         () => NegativeMassLevel(),
+        () => OrbitAroundLevel(),
       ];
 
   int get levelCount => _levels.length;
--- lib/data/levels/schwardschild_level.dart
+++ lib/data/levels/schwardschild_level.dart
@@ -7,7 +7,7 @@ import 'package:space_balls/model/target.dart';
 class SchwardschildLevel extends GameLevel {
   SchwardschildLevel()
       : super(
-          id: 'schwardschild',
+          id: 'schwarzschild',
           name: 'Schwarzschild black hole',
           description:
               'The simplest black hole is a spherically symmetric static black hole called the Schwarzschild black hole. Its gravity is so strong that a horizon exist where nothing can escape the immense force. Be careful so you dont fall in!',
