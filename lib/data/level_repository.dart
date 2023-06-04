29dbe78fe56ad1e61af1002d8ad6f5b94028c655
David Vokrouhlicky <david.vokrouhlicky@gmail.com>
2023-06-04 15:32:46 +0200

    FZU level, titles

--- lib/data/level_repository.dart
+++ lib/data/level_repository.dart
@@ -11,6 +11,7 @@ import 'package:space_balls/data/levels/variable_gravity_level.dart';
 import 'package:space_balls/data/levels/wall_bounce_level.dart';
 import 'package:space_balls/data/levels/negative_mass_level.dart';
 import 'package:space_balls/data/levels/orbit_around_level.dart';
+import 'package:space_balls/data/levels/fzu_level.dart';
 import 'package:space_balls/model/game_level.dart';
 
 
@@ -18,16 +19,17 @@ class LevelRepository {
   static List<GameLevel Function()> get _levels => [
         () => TestLevel(),
         () => TutorialLevel(),
+        () => WallBounceLevel(),
         () => NewtonianLevel(),
         () => SunWithPlanetLevel(),
         () => SchwardschildLevel(),
         () => KerrLevel(),
         () => BlackHoleMergerLevel(),
-        () => WallBounceLevel(),
         () => VariableGravityLevel(),
         () => SlalomLevel(),
         () => NegativeMassLevel(),
         () => OrbitAroundLevel(),
+        () => FzuLevel(),
       ];
 
   int get levelCount => _levels.length;
