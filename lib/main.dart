80c1caf3580f8676e8a102543f04bd677e71f84f
Jan Bouček <jan@janboucek.com>
2024-03-23 11:07:26 +0100

    2024 maintenance

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:logging/logging.dart';
 import 'package:space_balls/business/base_provider.dart';
@@ -7,7 +8,6 @@ import 'package:space_balls/business/user_bloc.dart';
 import 'package:space_balls/run_config.dart';
 import 'package:space_balls/ui/game_page.dart';
 import 'package:space_balls/ui/home_page.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   Logger.root.level = Level.ALL;
@@ -29,7 +29,16 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           title: 'Orbit Bound',
           theme: ThemeData(
-            primarySwatch: Colors.blue,
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.blue,
+              brightness: Brightness.dark,
+              primaryContainer: Colors.blue,
+              primary: Colors.white,
+            ),
+            appBarTheme: const AppBarTheme(
+              color: Colors.blue,
+              foregroundColor: Colors.white,
+            ),
           ),
           localizationsDelegates: const [
             AppLocalizations.delegate,
--- lib/ui/level_description_dialog.dart
+++ lib/ui/level_description_dialog.dart
@@ -13,25 +13,28 @@ class LevelDescriptionDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => Navigator.of(context).pop(),
-      child: Center(
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          color: const Color(0xFF2D2D2D),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                level.name,
-                style: const TextStyle(fontSize: 20, color: Colors.white),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                level.description,
-                style: const TextStyle(color: Colors.white),
-                textAlign: TextAlign.justify,
-              ),
-            ],
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            color: const Color(0xFF2D2D2D),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  level.name,
+                  style: const TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  level.description,
+                  style: const TextStyle(color: Colors.white),
+                  textAlign: TextAlign.justify,
+                ),
+              ],
+            ),
           ),
         ),
       ),
